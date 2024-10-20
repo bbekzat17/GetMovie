10deb65f5d774587e924c91f28ae00d116859e99
Bekzat <batyrkhanovbekzat0@gmail.com>
2024-10-20 12:34:38 +0500

    FIRST COMMIT

---
+++ GetMovie/Sources/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  GetMovie
+//
+//  Created by Bekzat Batyrkhanov on 20.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+}
+
